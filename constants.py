b650004f6b80a4b65da8afdfe511b448c9360e73
ngdnhtien <ngdnhtien_t65@hus.edu.vn>
2024-03-18 15:50:40 +0800

    add elementary elements

---
+++ constants.py
@@ -0,0 +1,5 @@
+import numpy as np 
+
+h = 6.6261e-34
+hbar = h/(2*np.pi)
+electron_charge = 1.602e-19
---
+++ elementary_functions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+pi = np.pi
+cos = np.cos
+sin = np.sin
+sqrt = np.sqrt
+conj = np.conjugate
